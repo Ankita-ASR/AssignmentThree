c984f7b6aaddaa1c94293114c758e0fd101d1f6a
Ankita Saha <ankita7saha@gmail.com>
2024-07-14 02:04:20 +0600

    //E-5

--- src/CountDigit.java
+++ src/CountDigit.java
@@ -1,6 +1,3 @@
-/*Write a program that reads an integer and prints how many digits the number has, by
-checking whether the number is ≥ 10, ≥ 100, and so on. (Assume that all integers are
-less than ten billion.) If the number is negative, first multiply it with –1.*/
 import java.util.*;
 public class CountDigit {
     public static void main(String [] args){
